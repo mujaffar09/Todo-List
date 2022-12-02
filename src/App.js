a8c23cc05af34728c59438ca202584df85c9496f
mujaffar09 <110367256+mujaffar09@users.noreply.github.com>
2022-12-02 20:24:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Todo from "./components/Todo";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Todo Appication</h1>
+      <Todo />
+    </div>
+  );
+}
